28d3cc770d009797a74f1e5ef59dfab24a7dfb25
Aki <kydovQWR@gmail.com>
2021-11-03 17:12:23 +0900

    要求仕様書v0.9.0に対応した。

--- src/ImageScreener/MoveImageToSelectedFolder.cs
+++ src/ImageScreener/MoveImageToSelectedFolder.cs
@@ -37,14 +37,37 @@ namespace ImageScreener
                     fs.Dispose();
                 }
 
-                Task.Delay(200);  // ストリームを閉じてもファイルへの接続が維持されているようなので、試しに200ms待機して完全なクローズを待ってみる。
+                // Task.Delay(200);  // ストリームを閉じてもファイルへの接続が維持されているようなので、試しに200ms待機して完全なクローズを待ってみる。
 
                 foreach (CheckBox cb in checkboxes)
                 {
                     if (cb.IsChecked == true)
                     {
+                        // 移動対象のターゲット画像が存在していることを記録する。
                         isCheckedExists = true;
-                        File.Move($"./{cb.Content.ToString()}", $"./{subFolderesList.SelectedItem}/{cb.Content.ToString()}");
+
+                        // 同名のファイルが移動先に存在している場合、ファイル名の末尾に"="を付与して移動する。
+                        // 例えば、"abdc.jpg"は"abcd=.jpg"に改名とする。
+                        string pathFrom = $"./{cb.Content.ToString()}";
+                        string pathCanditate = $"./{subFolderesList.SelectedItem}/{cb.Content.ToString()}";
+                        string pathTo = "";
+                        
+                        bool checkingPathCanditate = true;
+                        while (checkingPathCanditate == true)
+                        {
+                            if (File.Exists(pathCanditate) == true)
+                            {
+                                pathCanditate = $"./{subFolderesList.SelectedItem}/{Path.GetFileNameWithoutExtension(pathCanditate)}={Path.GetExtension(pathCanditate)}";
+                            }
+                            else
+                            {
+                                checkingPathCanditate = false;
+                            }
+                        }
+
+                        pathTo = pathCanditate;
+
+                        File.Move(pathFrom, pathTo);
                     }
                 }
 
